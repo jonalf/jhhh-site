567ee38904c81d58f8b77d62487ef0c138062b24
JonAlf <jonalf@gmail.com>
2016-08-08 15:03:00 -0400

    added ability to update the worst

--- jhh/add_episode.py
+++ jhh/add_episode.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 from sys import argv
 
-KEY = '<h2>Season 2</h2>'
+KEY = ''
 
-if len(argv) != 3:
-    print 'please provide a title and a link'
+if len(argv) != 4:
+    print 'Usage: SEASON #|worst TITLE LINK'
 else:
     embed_div = """<div class="media-container">
 <h3>NAME</h3>
@@ -13,22 +13,32 @@ FRAME
 </div>
 </div>
 """
-    embed_div = embed_div.replace("NAME", argv[1])
-    embed_div = embed_div.replace("FRAME", argv[2])
+    if argv[1] == 'worst':
+        KEY = '<h2>The Worst</h2>'
+    else:
+        KEY = '<h2>Season ' + argv[1] + '</h2>'
+    
+    embed_div = embed_div.replace("NAME", argv[2])
+    embed_div = embed_div.replace("FRAME", argv[3])
     print 'This is the div to be added:\n'
     print embed_div
     choice = raw_input('\nIs this ok? (y/n): ')
     if choice == 'y':
-        f = open('templates/episodes.html')
+        fname = ''
+        if argv[1] == 'worst':
+            fname = 'templates/worst.html'
+        else:
+            fname = 'templates/episodes.html'
+        f = open( fname )
         s = f.read()
         f.close()
         position = s.find(KEY) + len(KEY) + 2
         new_s = s[:position] + embed_div + s[position:]
         #backup episodes file just in case
-        f = open('templates/episodes-old.html', 'w')
+        f = open(fname + '.backup', 'w')
         f.write(s)
         f.close()
-        f = open('templates/episodes.html', 'w')
+        f = open(fname, 'w')
         f.write(new_s)
         f.close()
         print 'Done'
